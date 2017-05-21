c0d07b2f7e952ebc1494a4833adcc6899b7770aa
kattogat <maria_hav@live.se>
2017-05-21 17:48:23 +0200

    add

---
+++ app/model/pantry.php
@@ -0,0 +1,52 @@
+<?php
+require "../config.php";
+require "modeler.php";
+require "../controler.php";
+
+class PantryModel extends Model {
+	private $table = "pantry";
+	private $name;
+	private $qun;
+	private $liter;
+	private $kg;
+	
+	
+	public function __construct($name, $qun, $liter = "", $kg = "") {
+		$this->name = $name;
+		$this->qun = $qun;
+		$this->liter = $liter;
+		$this->kg = $kg;
+	}
+	
+	public function addProduct() {
+		require "../config.php";
+		
+		$sql = "INSERT INTO $this->table (name, quantity, liter, kg)
+            VALUES(:nameuo, :quntuo, :literuo, :kguo)";
+		$pantry_intoDb = $pdo->prepare($sql);
+		$pantry_intoDb- >execute (array(':nameuo' => $this->name, ':quntuo' => $this->qun, ':literuo' => $this->liter, ':kguo' => $this->kg)); 
+		
+		$done = true;
+		return $done;
+	}
+	
+	public function updateProduct() {
+		$sql2 = "UPDATE pantry SET lossenord = :olduo WHERE anvandarnamn = :newuo";
+		$uppdate = $pdo->prepare($sql2);
+		$uppdate->execute (array(':olduo' => $old, ':newuo' => $new)); 
+		
+	}
+	
+	public function deleteProduct() {
+		require "../config.php";
+		$sql = "DELETE FROM $this->table WHERE (name=:deluo)";
+		$bort = $pdo->prepare($sql);
+		$bort->execute (array(':deluo' => $this->name));
+		
+		$done = true;
+		return $done;
+	}
+}
+
+$test = new PantryModel($conName, $conQun, $conLiter, $conKg);
+$test->addProduct(); 
