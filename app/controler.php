eda7fdbec2ea4ec094a1563c7da291cafcadbae7
kattogat <maria_hav@live.se>
2017-05-21 17:39:05 +0200

    add

---
+++ app/controler.php
@@ -0,0 +1,8 @@
+<?php
+
+if(isset($_POST['pantryAdd'])) {	
+	$conName = $_POST['productName']; 
+	$conQun = $_POST['productQun']; 
+	$conLiter = $_POST['productLiter']; 
+	$conKg = $_POST['productKg']; 
+}
